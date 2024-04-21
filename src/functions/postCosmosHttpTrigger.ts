80dd33ced9ab53786d2c31c42e1ab674e952e0d6
thiago_lins <thiagopls1@hotmail.com>
2024-04-21 18:22:14 -0300

    Add postCosmosHttpTrigger

---
+++ src/functions/postCosmosHttpTrigger.ts
@@ -0,0 +1,29 @@
+import {
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+  input,
+} from "@azure/functions";
+import { Bookmark } from "../interfaces/bookmark";
+import { cosmosOutput } from "../outputs/cosmos-output";
+
+export async function postCosmosHttpTrigger(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+  const bookmark: Bookmark = await request.json() as Bookmark;
+  context.log(bookmark);
+  if (!bookmark) {
+    return {
+      status: 400,
+      body: "Bookmark must be set at request body",
+    };
+  }
+
+  context.extraOutputs.set(cosmosOutput, bookmark)
+
+  return {
+    body: `Posted bookmark ${bookmark.id}`,
+  };
+}
