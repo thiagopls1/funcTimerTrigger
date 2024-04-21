6ca17b8ad5e4754958e3f82caa96250248f80e68
thiago_lins <thiagopls1@hotmail.com>
2024-04-21 18:25:03 -0300

    Add new HttpTriggers

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,40 @@
-import { app } from '@azure/functions'
-import { timerTrigger } from './functions/timerTrigger' 
-import { httpTrigger } from './functions/httpTrigger';
-import { storageBlobTrigger } from './functions/storageBlobTrigger';
+import { app } from "@azure/functions";
+import { timerTrigger } from "./functions/timerTrigger";
+import { httpTrigger } from "./functions/httpTrigger";
+import { storageBlobTrigger } from "./functions/storageBlobTrigger";
+import { getCosmosHttpTrigger } from "./functions/getCosmosHttpTrigger";
+import { cosmosInput } from "./inputs/cosmos-input";
+import { postCosmosHttpTrigger } from "./functions/postCosmosHttpTrigger";
+import { cosmosOutput } from "./outputs/cosmos-output";
 
-app.timer('timerTrigger', {
-  schedule: '*/20 * * * * *',
-  handler: timerTrigger
-})
-
-
-app.http('httpTrigger', {
-  methods: ['GET', 'POST'],
-  authLevel: 'anonymous',
-  handler: httpTrigger
+app.timer("timerTrigger", {
+  schedule: "*/20 * * * * *",
+  handler: timerTrigger,
 });
 
-app.storageBlob('storageBlobTrigger', {
-  path: 'sample-workitems/{name}',
+app.http("httpTrigger", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: httpTrigger,
+});
+
+app.storageBlob("storageBlobTrigger", {
+  path: "sample-workitems/{name}",
   connection: "AzureWebJobsStorage",
   handler: storageBlobTrigger,
-})
+});
+
+app.http("getCosmosHttpTrigger", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  extraInputs: [cosmosInput],
+  route: 'getCosmosHttpTrigger/{id}',
+  handler: getCosmosHttpTrigger,
+});
+
+app.http("postCosmosHttpTrigger", {
+  methods: ["POST"],
+  authLevel: "anonymous",
+  handler: postCosmosHttpTrigger,
+  extraOutputs: [cosmosOutput]
+});
