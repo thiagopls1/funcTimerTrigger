1398ad844a3f71dd941446530131a166084cf872
thiago_lins <thiagopls1@hotmail.com>
2024-04-21 14:28:57 -0300

    Add httpTrigger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { app } from '@azure/functions'
 import { timerTrigger } from './functions/timerTrigger' 
+import { httpTrigger } from './functions/httpTrigger';
 
 app.timer('timerTrigger', {
   schedule: '*/20 * * * * *',
   handler: timerTrigger
 })
+
+
+app.http('httpTrigger', {
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  handler: httpTrigger
+});
