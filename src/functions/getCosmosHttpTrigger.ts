b6b54ab706268385eecf6cf8664b0dad7474bacc
thiago_lins <thiagopls1@hotmail.com>
2024-04-21 18:21:53 -0300

    Add getCosmosHttpTrigger

---
+++ src/functions/getCosmosHttpTrigger.ts
@@ -0,0 +1,27 @@
+import {
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+  input,
+} from "@azure/functions";
+import { Bookmark } from "../interfaces/bookmark";
+import { cosmosInput } from "../inputs/cosmos-input";
+
+export async function getCosmosHttpTrigger(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+  const bookmarkItem = <Bookmark>context.extraInputs.get(cosmosInput);
+  context.log(context.extraInputs);
+  if (!bookmarkItem) {
+    return {
+      status: 404,
+      body: "Bookmark not found",
+    };
+  }
+
+  return {
+    body: `Found bookmark item: Id: ${bookmarkItem.id}, Url: ${bookmarkItem.url}`,
+  };
+}
